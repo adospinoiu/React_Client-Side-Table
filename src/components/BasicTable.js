738ed31558fc52f34d8b7dae6acbde2b6c391252
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2021-01-15 06:36:44 -0700

    Created BasicTable.js file. Imported columns and data

---
+++ src/components/BasicTable.js
@@ -0,0 +1,52 @@
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
+import MOCK_DATA from './MOCK_DATA.json';
+import { COLUMNS } from './columns';
+
+export const BasicTable = () => {
+    
+    // By using the useMemo() you ensure that the table is not re-rendered every time. Otherwise, react would think it is receiving new data every time.
+    const columns = useMemo(() => COLUMNS, [])
+    const data = useMemo(() => MOCK_DATA, [])
+
+
+    const tableInstance = useTable({
+        // Here you are passing COLUMNS & MOCK_DATA for useTable() to use as the 'columns and data'
+        columns: COLUMNS,
+        data: MOCK_DATA
+
+        // *Shorter Version (ES6)*
+        // columns,
+        // data
+    })
+
+    // These are functions from the useTable HOOK from the react-table-package that are provided that can be accessed to create a table
+    const {
+        getTableProps,          // This is a function
+        getTableBodyProps,      // This is a function
+        headerGroups,           // This is an array
+        rows,
+        prepareRow,
+    } = tableInstance
+
+    return (
+        <div>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- src/components/columns.js
+++ src/components/columns.js
@@ -1,3 +1,7 @@
+// These are the columns that will be rendered to the client
+// The 'Header' is the title of the column that will show up on the client side
+// The 'accessor' is the WAY the data will be accessed from the MOCK_DATA.json file
+
 export const COLUMNS = [
     {
         Header: 'Id',
